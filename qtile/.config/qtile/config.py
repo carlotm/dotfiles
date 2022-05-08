de162bcf628e48af5111b00c3421058bb5d17ba9
Ascani Carlo <carlotm@pm.me>
2022-05-08 11:56:44 +0200

    Send mouse to focused window

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,3 +1,7 @@
+from libqtile.hook import subscribe
+import os
+import subprocess
+
 from conf.settings import *
 from conf.screens import screens as s
 from conf.keys import keys as k
@@ -5,9 +9,16 @@ from conf.layouts import layouts as l
 from conf.mouse import conf_mouse
 from conf.groups import groups as g
 
+home = os.path.expanduser("~")
 mod = "mod1"
 groups = g
 keys = k(mod, groups)
 layouts = l
 screens = s
 mouse = conf_mouse(mod)
+
+
+@subscribe.client_focus
+def send_mouse_to_focused(_):
+    cmd = f"{home}/.config/qtile/send_mouse_to_focused.sh"
+    subprocess.call([cmd])
