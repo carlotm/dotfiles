b19e6b1f810c1b71ef692972f1f1a7267972d51e
Ascani Carlo <carlo@doloops.net>
2023-07-17 07:37:24 +0200

    Update vim colors

--- config/.config/nvim/lua/keymaps.lua
+++ config/.config/nvim/lua/keymaps.lua
@@ -4,3 +4,5 @@ vim.api.nvim_set_keymap("v", ">", ">>", { noremap = true, silent = true });
 vim.api.nvim_set_keymap("v", "<", "<<", { noremap = true, silent = true });
 vim.api.nvim_set_keymap("n", "gp", ":bprev<CR>", { silent = true });
 vim.api.nvim_set_keymap("n", "gn", ":bnext<CR>", { silent = true });
+vim.api.nvim_set_keymap("n", "<Leader>ww", ":wincmd w<CR>", { silent = true });
+vim.api.nvim_set_keymap("n", "<Leader>bd", ":bp|bd #<CR>", { silent = true });
