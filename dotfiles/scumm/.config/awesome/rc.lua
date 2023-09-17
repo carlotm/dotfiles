50faabf6e9e251783e57037f03ae58d7f94348fa
Ascani Carlo <carlotm@protonmail.com>
2023-09-17 12:23:09 +0200

    Add autorandr and udev rule

--- dotfiles/scumm/.config/awesome/rc.lua
+++ dotfiles/scumm/.config/awesome/rc.lua
@@ -101,23 +101,24 @@ root.keys(globalkeys)
 
 -- {{{ Rules
 awful.rules.rules = {
-	{
-		rule = { },
-		properties =
-			{ border_width = beautiful.border_width
-			, border_color = beautiful.border_normal
-			, focus = awful.client.focus.filter
-			, raise = true
-			, keys = clientkeys
-			, buttons = clientbuttons
-			, screen = awful.screen.preferred
-			, placement = awful.placement.no_overlap+awful.placement.no_offscreen
-			}
+	{ rule = { }
+	, properties =
+		{ border_width = beautiful.border_width
+		, border_color = beautiful.border_normal
+		, focus = awful.client.focus.filter
+		, raise = true
+		, keys = clientkeys
+		, buttons = clientbuttons
+		, screen = awful.screen.preferred
+		, placement = awful.placement.no_overlap+awful.placement.no_offscreen
+		}
 	},
-	{
-		rule_any = { type = { "normal", "dialog" } },
-		properties = { titlebars_enabled = false }
+	{ rule_any = { type = { "normal", "dialog" } }
+	, properties = { titlebars_enabled = false }
 	},
+	{ rule = { instance = "xfce4-panel" }
+	, properties = { border_width = false }
+	}
 }
 -- }}}
 
