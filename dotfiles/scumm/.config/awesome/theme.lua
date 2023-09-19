e6eebd567e18be93c462897da078a39537bcc7da
Ascani Carlo <carlotm@protonmail.com>
2023-09-19 17:07:27 +0200

    Add spleen font

--- dotfiles/scumm/.config/awesome/theme.lua
+++ dotfiles/scumm/.config/awesome/theme.lua
@@ -7,7 +7,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font					= "Spleen 14"
+theme.font					= "Spleen 12"
 theme.bg_normal				= "#222222"
 theme.bg_focus				= "#535d6c"
 theme.bg_urgent				= "#ff0000"
