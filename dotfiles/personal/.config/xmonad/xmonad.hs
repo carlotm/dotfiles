4de14efa8c0cd09c21ea100b507d0d6d9b8c7da7
Ascani Carlo <carlo@doloops.net>
2024-01-03 11:56:27 +0100

    Add xmonad

---
+++ dotfiles/personal/.config/xmonad/xmonad.hs
@@ -0,0 +1,17 @@
+import XMonad
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Ungrab
+import XMonad.Hooks.EwmhDesktops
+
+main :: IO ()
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+
+myConfig = def
+    { terminal    = "alacritty"
+    , modMask     = mod1Mask
+    , borderWidth = 2
+    , layoutHook  = myLayout
+    }
+
+myLayout = Tall 1 (3/100) (1/2)
