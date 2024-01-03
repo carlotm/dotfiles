df455ea34d1155c509dd4fd16f13c56c1b3618d4
Ascani Carlo <carlo@doloops.net>
2024-01-03 12:08:51 +0100

    Remove awesome

--- dotfiles/personal/.config/xmonad/xmonad.hs
+++ dotfiles/personal/.config/xmonad/xmonad.hs
@@ -11,6 +11,8 @@ myConfig = def
     { terminal    = "alacritty"
     , modMask     = mod1Mask
     , borderWidth = 2
+    , normalBorderColor = "#222222"
+    , focusedBorderColor = "#ff0000"
     , layoutHook  = myLayout
     }
 
