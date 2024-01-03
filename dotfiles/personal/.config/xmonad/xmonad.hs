56a9f3f0fadd8b54c61bfa7df66a37f643a41ba9
Ascani Carlo <carlo@doloops.net>
2024-01-03 13:06:55 +0100

    Put application finder in the middle of the screen

--- dotfiles/personal/.config/xmonad/xmonad.hs
+++ dotfiles/personal/.config/xmonad/xmonad.hs
@@ -1,20 +1,30 @@
 import XMonad
 
+import XMonad.StackSet
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
 
 myConfig = def
-    { terminal    = "alacritty"
-    , modMask     = mod1Mask
+    { terminal = "alacritty"
+    , modMask = mod1Mask
     , borderWidth = 2
     , normalBorderColor = "#222222"
     , focusedBorderColor = "#ff0000"
-    , layoutHook  = myLayout
+    , layoutHook = myLayout
+    , manageHook = myManageHook
     }
 
 myLayout = smartBorders $ Tall 1 (3/100) (1/2)
+
+-- , title =?  "Application Finder" --> doCenterFloat
+myManageHook = composeAll
+    [ isDialog --> doCenterFloat
+    , isFullscreen --> doFullFloat
+    , title =?  "Application Finder" --> doRectFloat (RationalRect 0.2 0.2 0.6 0.6)
+    ]
