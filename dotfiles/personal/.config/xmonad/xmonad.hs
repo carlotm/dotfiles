70db05bad22ae86bbe836e28a77938b13d62f253
Ascani Carlo <carlo@doloops.net>
2024-01-05 10:30:57 +0100

    Use xfce + xmonad

--- dotfiles/personal/.config/xmonad/xmonad.hs
+++ dotfiles/personal/.config/xmonad/xmonad.hs
@@ -6,25 +6,35 @@ import XMonad.Util.Ungrab
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Config.Xfce
 
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
-
-myConfig = def
+main = xmonad xfceConfig
     { terminal = "lxterminal"
     , modMask = mod1Mask
     , borderWidth = 2
     , normalBorderColor = "#222222"
     , focusedBorderColor = "#ff0000"
-    , layoutHook = myLayout
-    , manageHook = myManageHook
+    , layoutHook = avoidStruts $ myLayout
     }
 
 myLayout = smartBorders $ Tall 1 (3/100) (1/2)
 
--- , title =?  "Application Finder" --> doCenterFloat
-myManageHook = composeAll
-    [ isDialog --> doCenterFloat
-    , isFullscreen --> doFullFloat
-    , title =?  "Application Finder" --> doRectFloat (RationalRect 0.2 0.2 0.6 0.6)
-    ]
+-- main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+
+-- myConfig = def
+--     { terminal = "lxterminal"
+--     , modMask = mod1Mask
+--     , borderWidth = 2
+--     , normalBorderColor = "#222222"
+--     , focusedBorderColor = "#ff0000"
+--     , layoutHook = avoidStruts $ myLayout
+--     , manageHook = manageDocks <+> myManageHook
+--     }
+
+-- myManageHook = composeAll
+--     [ isDialog --> doCenterFloat
+--     , isFullscreen --> doFullFloat
+--     , title =?  "Application Finder" --> doRectFloat (RationalRect 0.2 0.2 0.6 0.6)
+--     ]
